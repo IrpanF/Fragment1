781ddd26b30fed31f045a533547ceec9335639f2
Irpan F <88434415+IrpanF@users.noreply.github.com>
2023-08-08 11:19:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/Fragment/IntroActivity.java
@@ -0,0 +1,26 @@
+package com.example.Fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.Fragment.R;
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        ConstraintLayout introBtn = findViewById(R.id.introBtn);
+        introBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
